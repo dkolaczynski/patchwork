a5ae434cb7d5b2fa699f3bcafeb8c8653c26bb4b
Damian Kołaczyński <dkolaczynski@users.noreply.github.com>
2019-02-06 19:05:24 +0100

    'key-binder': migrated to PyGObject

--- applications/key-binder.py
+++ applications/key-binder.py
@@ -1,47 +1,23 @@
 #!/usr/bin/env python3
 
-import ast
 import json
-import subprocess
-from enum import Enum
+
+from gi.repository import Gio  # PyGObject should be available by default
 
 # TODO
 # - [FEATURE] Option for clearing custom keybindings: -c clear
 # - [FEATURE] Option to replace custom settings (by default it should append them):  -o overwrite, -a append
 # - [FEATURE] Option to dump existing custom keybindings: -d dump
 # - [FEATURE] Option for validation of configuration file: -t test
-# - https://lazka.github.io/pgi-docs/#Gio-2.0/classes/Settings.html
 
 MEDIA_KEYS_SCHEMA = 'org.gnome.settings-daemon.plugins.media-keys'
 CUSTOM_KEYBINDINGS_PATH = '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom{}/'
-CUSTOM_KEYBINDINGS_SCHEMA = MEDIA_KEYS_SCHEMA + '.custom-keybinding:' + CUSTOM_KEYBINDINGS_PATH
+CUSTOM_KEYBINDINGS_SCHEMA = MEDIA_KEYS_SCHEMA + '.custom-keybinding'
 
-
-class GsettingsKey(Enum):
-	NAME = 'name'
-	COMMAND = 'command'
-	BINDING = 'binding'
-	CUSTOM_KEYBINDINGS = 'custom-keybindings'
-
-
-def get_from_gsettings(schema, key):
-	completed_process = _run_gsettings('get', schema, key.value)
-	return completed_process.stdout.decode()
-
-
-def set_in_gsettings(schema, key, value):
-	_run_gsettings('set', schema, key.value, value)
-
-
-def _run_gsettings(*args):
-	command = ['gsettings']
-	command.extend(args)
-	return subprocess.run(command, stdout=subprocess.PIPE)  # In Python 3.7: capture_output=True
-
-
-def get_existing_custom_keybindings():
-	command_result = get_from_gsettings(MEDIA_KEYS_SCHEMA, GsettingsKey.CUSTOM_KEYBINDINGS)
-	return [] if command_result.startswith('@as []') else ast.literal_eval(command_result)
+NAME_KEY = 'name'
+COMMAND_KEY = 'command'
+BINDING_KEY = 'binding'
+CUSTOM_KEYBINDINGS_KEY = 'custom-keybindings'
 
 
 def load_configuration():
@@ -50,18 +26,22 @@ def load_configuration():
 
 
 def main():
-	configuration = load_configuration()
-	custom_keybindings = get_existing_custom_keybindings()
+	new_bindings = load_configuration()
+	existing_bindings = Gio.Settings(MEDIA_KEYS_SCHEMA).get_strv(CUSTOM_KEYBINDINGS_KEY)
 
-	for index, binding in enumerate(configuration, len(custom_keybindings)):
-		schema = CUSTOM_KEYBINDINGS_SCHEMA.format(index)
+	# Iterate through each defined keybinding
+	for index, binding in enumerate(new_bindings, len(existing_bindings)):
+		path = CUSTOM_KEYBINDINGS_PATH.format(index)
+		settings = Gio.Settings.new_with_path(CUSTOM_KEYBINDINGS_SCHEMA, path)
 
-		custom_keybindings.append(CUSTOM_KEYBINDINGS_PATH.format(index))
+		# Populate required data: name command and binding of each keybinding
+		for key in (NAME_KEY, COMMAND_KEY, BINDING_KEY):
+			settings.set_string(key, binding[key])
+			Gio.Settings.sync()
 
-		for key in (GsettingsKey.NAME, GsettingsKey.COMMAND, GsettingsKey.BINDING):
-			set_in_gsettings(schema, key, binding[key.value])
+		existing_bindings.append(path)
 
-	set_in_gsettings(MEDIA_KEYS_SCHEMA, GsettingsKey.CUSTOM_KEYBINDINGS, str(custom_keybindings))
+	Gio.Settings(MEDIA_KEYS_SCHEMA).set_strv(CUSTOM_KEYBINDINGS_KEY, existing_bindings)
 
 
 if __name__ == '__main__':
