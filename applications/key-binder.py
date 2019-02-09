c65b40f3bc87c1538f76e50562f1b5748379d486
Damian Kołaczyński <dkolaczynski@users.noreply.github.com>
2019-02-09 14:58:25 +0100

    'key-binder': added option to delete all keyboard shortcuts

--- applications/key-binder.py
+++ applications/key-binder.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 import json
+from argparse import ArgumentParser
 
 from gi.repository import Gio  # PyGObject should be available by default
 
 # TODO
-# - [FEATURE] Option for clearing custom keybindings: -c clear
 # - [FEATURE] Option to replace custom settings (by default it should append them):  -o overwrite, -a append
 # - [FEATURE] Option to dump existing custom keybindings: -d dump
 # - [FEATURE] Option for validation of configuration file: -t test
@@ -14,10 +14,16 @@ MEDIA_KEYS_SCHEMA = 'org.gnome.settings-daemon.plugins.media-keys'
 CUSTOM_KEYBINDINGS_PATH = '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom{}/'
 CUSTOM_KEYBINDINGS_SCHEMA = MEDIA_KEYS_SCHEMA + '.custom-keybinding'
 
-NAME_KEY = 'name'
-COMMAND_KEY = 'command'
-BINDING_KEY = 'binding'
+KEYBINDING_KEYS = ('name', 'command', 'binding')
 CUSTOM_KEYBINDINGS_KEY = 'custom-keybindings'
+
+
+def parse_arguments(parser):
+	group = parser.add_mutually_exclusive_group()
+	group.add_argument('-a', '--append', action='store_true', help='TODO')
+	group.add_argument('-c', '--clear', action='store_true', help='TODO')
+
+	return parser.parse_args()
 
 
 def load_configuration():
@@ -25,7 +31,7 @@ def load_configuration():
 		return json.load(configuration)
 
 
-def main():
+def append_settings():
 	new_bindings = load_configuration()
 	existing_bindings = Gio.Settings(MEDIA_KEYS_SCHEMA).get_strv(CUSTOM_KEYBINDINGS_KEY)
 
@@ -35,13 +41,35 @@ def main():
 		settings = Gio.Settings.new_with_path(CUSTOM_KEYBINDINGS_SCHEMA, path)
 
 		# Populate required data: name command and binding of each keybinding
-		for key in (NAME_KEY, COMMAND_KEY, BINDING_KEY):
+		for key in KEYBINDING_KEYS:
 			settings.set_string(key, binding[key])
 			Gio.Settings.sync()
 
 		existing_bindings.append(path)
 
 	Gio.Settings(MEDIA_KEYS_SCHEMA).set_strv(CUSTOM_KEYBINDINGS_KEY, existing_bindings)
+
+
+def clear_settings():
+	existing_bindings = Gio.Settings(MEDIA_KEYS_SCHEMA)
+
+	for binding in existing_bindings.get_strv(CUSTOM_KEYBINDINGS_KEY):
+		setting = Gio.Settings(CUSTOM_KEYBINDINGS_SCHEMA, binding)
+
+		for key in KEYBINDING_KEYS:
+			setting.reset(key)
+			Gio.Settings.sync()
+
+	existing_bindings.reset(CUSTOM_KEYBINDINGS_KEY)
+
+
+def main():
+	args = parse_arguments(ArgumentParser(description="TODO"))
+
+	if args.append:
+		append_settings()
+	elif args.clear:
+		clear_settings()
 
 
 if __name__ == '__main__':
