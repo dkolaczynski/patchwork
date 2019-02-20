55c00396fef469da6bdd79aed089396700300956
Damian Kołaczyński <dkolaczynski@users.noreply.github.com>
2019-02-20 19:55:52 +0100

    'key-binder': added option to overwrite custom shortcuts

--- applications/key-binder.py
+++ applications/key-binder.py
@@ -6,7 +6,6 @@ from argparse import ArgumentParser
 from gi.repository import Gio  # PyGObject should be available by default
 
 # TODO
-# - [FEATURE] Option to replace custom settings (by default it should append them):  -o overwrite, -a append
 # - [FEATURE] Option to dump existing custom keybindings: -d dump
 # - [FEATURE] Option for validation of configuration file: -t test
 
@@ -21,6 +20,7 @@ CUSTOM_KEYBINDINGS_KEY = 'custom-keybindings'
 def parse_arguments(parser):
 	group = parser.add_mutually_exclusive_group()
 	group.add_argument('-a', '--append', action='store_true', help='TODO')
+	group.add_argument('-o', '--overwrite', action='store_true', help='TODO')
 	group.add_argument('-c', '--clear', action='store_true', help='TODO')
 
 	return parser.parse_args()
@@ -31,16 +31,24 @@ def load_configuration():
 		return json.load(configuration)
 
 
-def append_settings():
-	new_bindings = load_configuration()
-	existing_bindings = Gio.Settings(MEDIA_KEYS_SCHEMA).get_strv(CUSTOM_KEYBINDINGS_KEY)
+def overwrite_bindings():
+	_add_bindings([])
 
-	# Iterate through each defined keybinding
+
+def append_settings():
+	existing_bindings = Gio.Settings(MEDIA_KEYS_SCHEMA).get_strv(CUSTOM_KEYBINDINGS_KEY)
+	_add_bindings(existing_bindings)
+
+
+def _add_bindings(existing_bindings):
+	new_bindings = load_configuration()
+
+	# Iterate through each defined binding
 	for index, binding in enumerate(new_bindings, len(existing_bindings)):
 		path = CUSTOM_KEYBINDINGS_PATH.format(index)
 		settings = Gio.Settings.new_with_path(CUSTOM_KEYBINDINGS_SCHEMA, path)
 
-		# Populate required data: name command and binding of each keybinding
+		# Populate required data: name, command and binding of each bindings
 		for key in KEYBINDING_KEYS:
 			settings.set_string(key, binding[key])
 			Gio.Settings.sync()
@@ -66,8 +74,10 @@ def clear_settings():
 def main():
 	args = parse_arguments(ArgumentParser(description="TODO"))
 
-	if args.append:
+	if args.append:  # TODO make this option a default one
 		append_settings()
+	elif args.overwrite:
+		overwrite_bindings()
 	elif args.clear:
 		clear_settings()
 
