4a12251e75cfbc15ea94693b9119281b0bf1fb14
Damian Kołaczyński <dkolaczynski@users.noreply.github.com>
2019-02-25 20:46:22 +0100

    'key-binder': refactoring / validation

--- applications/key-binder.py
+++ applications/key-binder.py
@@ -5,9 +5,8 @@ from argparse import ArgumentParser
 
 from gi.repository import Gio  # PyGObject should be available by default
 
-# TODO
-# - [FEATURE] Option for validation of configuration file: -t test (or validate?)
-# - [FEATURE] Option for specifying input/output file; should not work widh -c: -f
+INPUT_FILENAME = './key-binder.json'
+OUTPUT_FILENAME = './key-binder-dump.json'
 
 MEDIA_KEYS_SCHEMA = 'org.gnome.settings-daemon.plugins.media-keys'
 CUSTOM_KEYBINDINGS_PATH = '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom{}/'
@@ -18,30 +17,37 @@ CUSTOM_KEYBINDINGS_KEY = 'custom-keybindings'
 
 
 def parse_arguments(parser):
-	group = parser.add_mutually_exclusive_group()
+	group = parser.add_mutually_exclusive_group(required=True)
 	group.add_argument('-a', '--append', action='store_true', help='TODO')
 	group.add_argument('-o', '--overwrite', action='store_true', help='TODO')
-	group.add_argument('-c', '--clear', action='store_true', help='TODO')
+	cleararg = group.add_argument('-c', '--clear', action='store_true', help='TODO')
 	group.add_argument('-d', '--dump', action='store_true', help='TODO')
+	group.add_argument('-v', '--validate', action='store_true', help='TODO')
+	filearg = parser.add_argument('-f', '--file', metavar='PATH', help='TODO')
 
-	return parser.parse_args()
+	args = parser.parse_args()
+
+	if args.clear and args.file:
+		parser.error('the combination of arguments {}/{} and {}/{} is not allowed'
+			.format(*cleararg.option_strings, *filearg.option_strings))
+	return args
 
 
-def load_configuration():
-	with open('./key-binder.json') as configuration:
+def load_configuration(path):
+	with open(path) as configuration:
 		return json.load(configuration)
 
 
-def overwrite_bindings():
-	_add_bindings([])
+def overwrite_bindings(path):
+	_add_bindings(path, [])
 
 
-def append_bindings():
+def append_bindings(path):
 	existing_bindings = Gio.Settings(MEDIA_KEYS_SCHEMA).get_strv(CUSTOM_KEYBINDINGS_KEY)
-	_add_bindings(existing_bindings)
+	_add_bindings(path, existing_bindings)
 
 
-def dump_bindings():
+def dump_bindings(path):
 	existing_bindings = Gio.Settings(MEDIA_KEYS_SCHEMA).get_strv(CUSTOM_KEYBINDINGS_KEY)
 	bindings = []
 
@@ -54,13 +60,13 @@ def dump_bindings():
 
 		bindings.append(binding)
 
-	with open('./key-binder-dump.json', 'w') as fp:
+	with open(path, 'w') as fp:
 		json.dump(bindings, fp, indent='\t')
 		fp.write('\n')
 
 
-def _add_bindings(existing_bindings):
-	new_bindings = load_configuration()
+def _add_bindings(path, existing_bindings):
+	new_bindings = load_configuration(path)
 
 	# Iterate through each defined binding
 	for index, binding in enumerate(new_bindings, len(existing_bindings)):
@@ -93,14 +99,14 @@ def clear_settings():
 def main():
 	args = parse_arguments(ArgumentParser(description="TODO"))
 
-	if args.append:  # TODO make this option a default one
-		append_bindings()
+	if args.append:
+		append_bindings(args.file if args.file else INPUT_FILENAME)
 	elif args.overwrite:
-		overwrite_bindings()
+		overwrite_bindings(args.file if args.file else INPUT_FILENAME)
 	elif args.clear:
 		clear_settings()
 	elif args.dump:
-		dump_bindings()
+		dump_bindings(args.file if args.file else OUTPUT_FILENAME)
 
 
 if __name__ == '__main__':
